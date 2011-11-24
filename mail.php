c5bfd65a1b25b055904a304623e667234d7f620c
Jelle de Jong <jelle@infoprojects.nl>
2011-11-24 11:27:37 +0100

    Multi-part fixes

--- mail.php
+++ mail.php
@@ -66,7 +66,7 @@ function sendmail($to) {
 
   $body .= "</body></html>";
 
-  return mail($to, $subject, $body, getheaders($_POST["Naam"], $_POST["E-mail"]));
+  return multipartmail($_POST["Naam"], $_POST["E-mail"], $to, $subject, $body);
 }
 
 // verstuur het 'bedankt'-mailtje
@@ -79,17 +79,36 @@ function sendthanks($to) {
   $body .= "<p>Uw bestelling (nr. " . $uid . ") is verzonden naar " . $emailaddress . ", waarvoor dank.</p>";
   $body .= "<p>M.v.g.,</p><p>Afzender<br />" . $emailaddress . "</p>";
 
-  return mail($to, $subject, $body, getheaders("Mediterranee Food", $emailaddress));
+  return multipartmail("Mediterranee Food", $emailaddress, $to, $subject, $body);
 }
 
-// standaard-headers voor de mail
-function getheaders($name, $from) {
-  $headers  = "From: \"" . strip_tags($name) . "\" <" . strip_tags($from) . ">\r\n";
-  $headers .= "Reply-To: " . strip_tags($from) . "\r\n";
-  $headers .= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+// multipart mail versturen
+function multipartmail($name, $from, $to, $subject, $html) {
+  $headers  = "From: \"" . strip_tags($name) . "\" <" . strip_tags($from) . ">\n";
+  $headers .= "Reply-To: " . strip_tags($from) . "\n";
 
-  return $headers;
+  $boundary = md5(date('r', time()));
+
+  $headers .= "Content-Type: multipart/alternative;";
+  $headers .= " boundary=\"phpMailer-" . $boundary . "\"\n";
+  $headers .= "MIME-Version: 1.0\n";
+  $headers .= "This is a MIME encoded message.\n";
+
+  // plain text part
+  $body  = "--phpMailer-" . $boundary . "\n";
+  $body .= "Content-Type: text/plain; charset=\"ISO-8859-1\"\n";
+  $body .= "Content-Transfer-Encoding: base64\r\n";
+  $body .= chunk_split(base64_encode(strip_tags(preg_replace('/<br(\s+)?\/?>/i', "\n", $html))));
+
+  // html part
+  $body .= "--phpMailer-" . $boundary . "\n";
+  $body .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n";
+  $body .= "Content-Transfer-Encoding: base64\r\n";
+  $body .= chunk_split(base64_encode($html));
+
+  $body .= "\n--phpMailer-" . $boundary . "--";
+
+  return mail($to, $subject, $body, $headers);
 }
 
 // redirect de pagina naar de opgegeven URL
